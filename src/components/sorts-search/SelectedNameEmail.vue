5d34ffd9f9f2a9acf3357445691e5aa814a605fe
Olga Budickaja <olgabudickaja@gmail.com>
2022-11-24 23:23:57 +0200

    add component Sorts Name and Email

---
+++ src/components/sorts-search/SelectedNameEmail.vue
@@ -0,0 +1,48 @@
+<template>
+  <MySelect
+      :selected.sync="$store.state.inputsModule.selectedNameEmail"
+      :options="sortNameEmails"
+      :selectDefault="selectDefault"
+      :iconDefault="iconDefault"
+      @change="selectedNameEmail"
+  >
+    <div>
+      <v-icon
+          class="text-h5 iconSelect"
+          left
+      >
+        {{ selectedNameEmail.icon }}
+      </v-icon>
+      {{ selectedNameEmail.name }}
+    </div>
+  </MySelect>
+</template>
+
+<script>
+import MySelect from "@/UI/MySelect";
+import { mapMutations, mapState } from "vuex";
+
+export default {
+  name: 'selected-name-email',
+  components: {MySelect},
+  data: () => ({
+    selectDefault: 'Sorts Users',
+    iconDefault: 'mdi-sort'
+  }),
+  methods: {
+    ...mapMutations({
+      setSelectedNameEmail: 'inputsModule/setSelectedNameEmail',
+    }),
+  },
+  computed: {
+    ...mapState({
+      selectedNameEmail: state => state.inputsModule.selectedNameEmail,
+      sortNameEmails: state => state.inputsModule.sortNameEmails,
+    }),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
