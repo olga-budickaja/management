7ddbfdc36f1d206637210318b39d7fd8a602a605
Olga Budickaja <olgabudickaja@gmail.com>
2022-11-24 23:08:29 +0200

    add component input Search

---
+++ src/components/sorts-search/SearchUsers.vue
@@ -0,0 +1,32 @@
+<template>
+  <my-input
+      :input.sync="searchQuery"
+      @update:input="setSearchQuery"
+      label="Search..."
+      class="mt-5"
+  />
+</template>
+
+<script>
+import MyInput from "@/UI/MyInput";
+import { mapMutations, mapState } from "vuex";
+
+export default {
+  name: 'search-user',
+  components: {MyInput},
+  methods: {
+    ...mapMutations({
+      setSearchQuery: 'inputsModule/setSearchQuery',
+    }),
+  },
+  computed: {
+    ...mapState({
+      searchQuery: state => state.inputsModule.searchQuery,
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
